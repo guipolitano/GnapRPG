18abb5fa1be6db1639c3cb7d6116901aa90680fd
Guilherme Politano de Sant' Anna <43428456+guipolitano@users.noreply.github.com>
2018-12-13 13:23:01 -0200

    Add files via upload

--- hangman-game/app.js
+++ hangman-game/app.js
@@ -1,22 +1,33 @@
-var palavras = ['adulto', "conselhos", "arranjo", "tentativa", "agosto", "outono", "fronteira", "brisa",
+/*
+ * PT-BR - Este jogo é o primeiro trabalho feito por Guilherme Politano de Sant'Anna como meio de
+ * aprendizado e estudo de Javascript, CSS e HTML.
+ *
+ * EN-BR - This game is the first work done by Guilherme Politano de Sant'Anna as a means of
+ * Learning and studying Javascript, CSS and HTML.
+ */
+
+var words = ['adulto', "conselhos", "arranjo", "tentativa", "agosto", "outono", "fronteira", "brisa",
   "tijolo", "calma", "canal", "escolheu", "garras", "treinador", "constantemente", "profundamente",
   "agarrado", "alegremente", "imagem", "independente", "instante", "janeiro", "manufatura", "derretido",
   "moinho", "macaco", "misterioso", "nozes", "oficial"
 ];
-var erros, erro, digitado, acertou, errou, sorteio, resposta, english = false;
+var wrongLetters, mistakes, input, correctBoolean, wrongBoolean, draw, answer, english = false;
 
 init();
+// ----------------------------------------------------------------------------
 /*
  * PT-BR - Troca do Idioma
  * EN-US - Language Change
  */
+// ----------------------------------------------------------------------------
 document.querySelector('.btn-en').addEventListener('click', function() {
   document.querySelector('.btn-en').style.fontWeight = "bold";
   document.querySelector('.btn-pt').style.fontWeight = "normal";
-  palavras = ['able', 'about', 'account', 'band', 'base', 'basin', 'basket', 'branch', 'brass', 'bread', 'breath',
-    'come', 'comfort', 'committee', 'common', 'company', 'desire', 'destruction', 'detail', 'development', 'different',
-    'digestion', 'experience', 'expert', 'eye', 'face', 'fact', 'fall', 'fruit', 'full', 'future', 'garden', 'general',
-    'get', 'girl'
+  words = ['able', 'about', 'account', 'band', 'base', 'basin', 'basket',
+    'branch', 'brass', 'bread', 'breath', 'come', 'comfort', 'committee', 'common',
+    'company', 'desire', 'destruction', 'detail', 'development', 'different',
+    'digestion', 'experience', 'expert', 'eye', 'face', 'fact', 'fall', 'fruit',
+    'full', 'future', 'garden', 'general', 'get', 'girl'
   ];
   document.getElementById('bank-text').textContent = 'Wrong Letters:';
   document.querySelector('.btn-submit').textContent = 'SEND';
@@ -27,10 +38,11 @@ document.querySelector('.btn-en').addEventListener('click', function() {
   english = true;
   init();
 });
+
 document.querySelector('.btn-pt').addEventListener('click', function() {
   document.querySelector('.btn-pt').style.fontWeight = "bold";
   document.querySelector('.btn-en').style.fontWeight = "normal";
-  palavras = ['adulto', "conselhos", "arranjo", "tentativa", "agosto", "outono", "fronteira", "brisa",
+  words = ['adulto', "conselhos", "arranjo", "tentativa", "agosto", "outono", "fronteira", "brisa",
     "tijolo", "calma", "canal", "escolheu", "garras", "treinador", "constantemente", "profundamente",
     "agarrado", "alegremente", "imagem", "independente", "instante", "janeiro", "manufatura", "derretido",
     "moinho", "macaco", "misterioso", "nozes", "oficial"
@@ -44,75 +56,94 @@ document.querySelector('.btn-pt').addEventListener('click', function() {
   english = false;
   init();
 });
+// ----------------------------------------------------------------------------
+/*
+ * PT-BR - Pega a letra inserida e inicia a função de verifiar o acerto
+ * EN-US - Get the insert letter and execute the function to verify the correctness
+ */
+// ----------------------------------------------------------------------------
 document.querySelector('.btn-submit').addEventListener('click', function() {
-  digitado = document.getElementById('letra').value.toLowerCase();
-  document.getElementById('letra').value = "";
-  verificarAcerto(digitado)
+  input = document.getElementById('letter').value.toLowerCase();
+  document.getElementById('letter').value = "";
+  checkCorrect(input)
 });
 
-function verificarAcerto(digitado) {
-  for (var i = 0; i < palavras[sorteio].length; i++) {
-    if (palavras[sorteio].charAt(i) === digitado) {
-      resposta[i] = digitado;
-      document.querySelector('.letras').textContent = resposta.join(' ');
-      acertou = true;
-      errou = false;
+// ----------------------------------------------------------------------------
+/*
+ * PT-BR - Função de verificar os acertos
+ * EN-US - Function to verify the correctness
+ */
+// ----------------------------------------------------------------------------
+
+function checkCorrect(input) {
+  for (var i = 0; i < words[draw].length; i++) {
+    if (words[draw].charAt(i) === input) {
+      answer[i] = input;
+      document.querySelector('.letters').textContent = answer.join(' ');
+      correctBoolean = true;
+      wrongBoolean = false;
     } else {
-      errou = true;
+      wrongBoolean = true;
     }
   }
 
-  if (acertou === false && errou === true) {
-    errou = true;
-    erros[erro] = digitado;
-    erro++;
-    document.querySelector('.hangman').src = 'hang-' + erro + '.png';
+  if (correctBoolean === false && wrongBoolean === true) {
+    wrongBoolean = true;
+    wrongLetters[mistakes] = input;
+    mistakes++;
+    document.querySelector('.hangman').src = 'hang-' + mistakes + '.png';
   }
 
-  if (erro > 0) {
-    for (var i = 0; i < erros.length; i++) {
-      document.querySelector('#erradas').style.display = 'block';
-      document.querySelector('#erradas').textContent = erros.join(' ');
+  if (mistakes > 0) {
+    for (var i = 0; i < wrongLetters.length; i++) {
+      document.querySelector('#wrongs').style.display = 'block';
+      document.querySelector('#wrongs').textContent = wrongLetters.join(' ');
     }
   }
 
-  if (erro === 6) {
+  if (mistakes === 6) {
     document.querySelector('.lose').style.display = 'block';
 
     if (english) {
-      document.querySelector('#word').textContent = 'The word was: ' + palavras[sorteio];
+      document.querySelector('#word').textContent = 'The word was: ' + words[draw];
     } else {
-      document.querySelector('#word').textContent = 'A palavra era: ' + palavras[sorteio];
+      document.querySelector('#word').textContent = 'A palavra era: ' + words[draw];
     }
     document.querySelector('.btn-submit').disabled = true;
-  } else if (palavras[sorteio] === resposta.join("")) {
+  } else if (words[draw] === answer.join("")) {
     document.querySelector('.winner').style.display = 'block';
     document.querySelector('.btn-submit').disabled = true;
   }
-  acertou = false;
+  correctBoolean = false;
 };
 
+// ----------------------------------------------------------------------------
+/*
+ * PT-BR - Botão de novo jogo
+ * EN-US - New Game button
+ */
+// ----------------------------------------------------------------------------
 document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
-  erros = ['', '', '', '', '', ''];
-  erro = 0;
-  acertou = false, errou = false;
-  document.querySelector('.hangman').src = 'hang-' + erro + '.png';
-  document.querySelector('.letras').textContent = ' ';
-  document.querySelector('#erradas').textContent = ' ';
+  wrongLetters = ['', '', '', '', '', ''];
+  mistakes = 0;
+  correctBoolean = false, wrongBoolean = false;
+  document.querySelector('.hangman').src = 'hang-' + mistakes + '.png';
+  document.querySelector('.letters').textContent = ' ';
+  document.querySelector('#wrongs').textContent = ' ';
   document.querySelector('.winner').style.display = 'none';
   document.querySelector('.btn-submit').disabled = false;
   document.querySelector('.lose').style.display = 'none';
   document.querySelector('#word').textContent = ' ';
 
-  sorteio = Math.floor(Math.random() * palavras.length);
-  resposta = [];
+  draw = Math.floor(Math.random() * words.length);
+  answer = [];
 
-  resposta.length = palavras[sorteio].length;
+  answer.length = words[draw].length;
 
-  for (var i = 0; i < resposta.length; i++) {
-    resposta[i] = '_';
-    document.querySelector('.letras').textContent += ' ' + resposta[i];
+  for (var i = 0; i < answer.length; i++) {
+    answer[i] = '_';
+    document.querySelector('.letters').textContent += ' ' + answer[i];
   }
 }
