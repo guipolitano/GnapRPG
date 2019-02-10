7c32351d4e2712b1bb28ebe09ab63750a791ebb7
guipolitano <gui_politano@hotmail.com>
2019-02-10 18:42:21 -0200

    Title change

--- Personal Projects/index.php
+++ Personal Projects/index.php
@@ -14,7 +14,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <title>App Help Desk</title>
+  <title>Avrae Attack Alias Generator</title>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
