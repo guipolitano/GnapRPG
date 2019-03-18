523d8f92f07a6e0b2bb99eb0f60b9a90a9347d24
guipolitano <gui_politano@hotmail.com>
2019-03-18 17:52:15 -0300

    bug correction

--- dnd/database/migrations/2014_10_12_000000_create_users_table.php
+++ dnd/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('username')->unique();
             $table->string('password');
-            $table->tinyInt('admin');
+            $table->tinyInteger('admin');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
